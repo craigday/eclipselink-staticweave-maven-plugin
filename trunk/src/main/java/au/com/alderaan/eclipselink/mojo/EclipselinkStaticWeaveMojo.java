cffa3ffdff08f67f2ffd9e3c48cb1ac210acc96f
craigday@gmail.com <craigday@gmail.com@643af56a-3fb0-4916-bea5-090f3e12a4de>
2012-12-04 01:09:10 +0000

    Workaround for Eclipselink/ASM classloader issue in < 2.3.3

--- trunk/src/main/java/au/com/alderaan/eclipselink/mojo/EclipselinkStaticWeaveMojo.java
+++ trunk/src/main/java/au/com/alderaan/eclipselink/mojo/EclipselinkStaticWeaveMojo.java
@@ -20,10 +20,12 @@ import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.classworlds.realm.ClassRealm;
 import org.eclipse.persistence.logging.AbstractSessionLog;
 import org.eclipse.persistence.logging.SessionLog;
 import org.eclipse.persistence.tools.weaving.jpa.StaticWeaveProcessor;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.MalformedURLException;
@@ -37,30 +39,29 @@ import java.util.Set;
 /**
  * Goal which performs Eclipselink static weaving.
  *
+ * @author Craig Day <craigday@gmail.com>
  * @goal weave
  * @phase process-classes
  * @requiresDependencyResolution compile
- *
- * @author Craig Day <craigday@gmail.com>
  */
 public class EclipselinkStaticWeaveMojo extends AbstractMojo {
     /**
-     * @parameter expression="${weave.persistenceInfo}
+     * @parameter expression="${weave.persistenceInfo}"
      */
     private String persistenceInfo;
 
     /**
-     * @parameter expression="${weave.persistenceXMLLocation}
+     * @parameter expression="${weave.persistenceXMLLocation}"
      */
     private String persistenceXMLLocation;
 
     /**
-     * @parameter expression="${project.build.outputDirectory}
+     * @parameter expression="${project.build.outputDirectory}"
      */
     private String source;
 
     /**
-     * @parameter expression="${project.build.outputDirectory}
+     * @parameter expression="${project.build.outputDirectory}"
      */
     private String target;
 
@@ -78,8 +79,30 @@ public class EclipselinkStaticWeaveMojo extends AbstractMojo {
      */
     private MavenProject project;
 
+    /**
+     * Include the project output directory and class path. This works around a bug in Eclipselink that uses
+     * {@link Class#forName(String)} without referencing a class loader that knows about project classes.
+     * Offending method is
+     * {@link org.eclipse.persistence.internal.libraries.asm.ClassWriter#getCommonSuperClass(String, String)}
+     * and should be fixed in Eclipselink 2.3.3.
+     *
+     * @parameter default-value="false"
+     */
+    private boolean includeProjectClasspath = false;
+
     public void execute() throws MojoExecutionException {
         try {
+            if (includeProjectClasspath) {
+                // Thread context class loader is the ClassRealm for this plugin.
+                ClassRealm c = (ClassRealm) Thread.currentThread().getContextClassLoader();
+                // Add Project output directory to class path.
+                c.addURL(new File(project.getBuild().getOutputDirectory()).toURI().toURL());
+                // Add Project class path to class path.
+                for (URL url : buildClassPath()) {
+                    c.addURL(url);
+                }
+            }
+
             StaticWeaveProcessor weave = new StaticWeaveProcessor(source, target);
             URL[] urls = buildClassPath();
             if (urls.length > 0) {
@@ -110,14 +133,14 @@ public class EclipselinkStaticWeaveMojo extends AbstractMojo {
 
     public void setLogLevel(String logLevel) {
         if (SessionLog.OFF_LABEL.equalsIgnoreCase(logLevel) ||
-            SessionLog.SEVERE_LABEL.equalsIgnoreCase(logLevel) ||
-            SessionLog.WARNING_LABEL.equalsIgnoreCase(logLevel) ||
-            SessionLog.INFO_LABEL.equalsIgnoreCase(logLevel) ||
-            SessionLog.CONFIG_LABEL.equalsIgnoreCase(logLevel) ||
-            SessionLog.FINE_LABEL.equalsIgnoreCase(logLevel) ||
-            SessionLog.FINER_LABEL.equalsIgnoreCase(logLevel) ||
-            SessionLog.FINEST_LABEL.equalsIgnoreCase(logLevel) ||
-            SessionLog.ALL_LABEL.equalsIgnoreCase(logLevel)) {
+                SessionLog.SEVERE_LABEL.equalsIgnoreCase(logLevel) ||
+                SessionLog.WARNING_LABEL.equalsIgnoreCase(logLevel) ||
+                SessionLog.INFO_LABEL.equalsIgnoreCase(logLevel) ||
+                SessionLog.CONFIG_LABEL.equalsIgnoreCase(logLevel) ||
+                SessionLog.FINE_LABEL.equalsIgnoreCase(logLevel) ||
+                SessionLog.FINER_LABEL.equalsIgnoreCase(logLevel) ||
+                SessionLog.FINEST_LABEL.equalsIgnoreCase(logLevel) ||
+                SessionLog.ALL_LABEL.equalsIgnoreCase(logLevel)) {
             this.logLevel = logLevel.toUpperCase();
         } else {
             throw new IllegalArgumentException("Unknown log level: " + logLevel);
@@ -133,5 +156,4 @@ public class EclipselinkStaticWeaveMojo extends AbstractMojo {
         }
         return urls.toArray(new URL[urls.size()]);
     }
-
 }
